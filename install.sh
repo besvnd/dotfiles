11eaabd58dad17b3c07e7c51989589e369bf07bd
Bendik Svendsen <bendik.svendsen@fremtind.no>
2025-09-03 22:59:44 +0200

    add zshrc and starship

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 stow -t ~ nvim
 stow -t ~ ghostty
 stow -t ~ tmux
+stow -t ~ starship
+stow -t ~ zshrc
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
---
+++ zshrc/.zshrc
@@ -0,0 +1,5 @@
+[ -f $HOME/opt/etc/shrc ] && . $HOME/opt/etc/shrc
+
+alias kubectl="kubecolor"
+
+eval "$(starship init zsh)"
