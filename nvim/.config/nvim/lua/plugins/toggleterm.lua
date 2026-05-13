221171a322b836ec1d77874d298099e0001bd577
Bendik Svendsen <bendik.svendsen@fremtind.no>
2026-05-13 13:56:34 +0200

    nvim: toggle copilot and terminal

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -5,4 +5,33 @@ return {
 		open_mapping = [[<C-`>]],
 		direction = "tab",
 	},
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
+
+		local Terminal = require("toggleterm.terminal").Terminal
+		local copilot = Terminal:new({
+			cmd = "gh copilot",
+			direction = "vertical",
+			hidden = true,
+			on_open = function(term)
+				vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<leader>cc", "<cmd>close<cr>", { silent = true })
+			end,
+		})
+
+		local term = Terminal:new({
+			direction = "horizontal",
+			hidden = true,
+			on_open = function(t)
+				vim.api.nvim_buf_set_keymap(t.bufnr, "t", "<leader>tt", "<cmd>close<cr>", { silent = true })
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>cc", function()
+			copilot:toggle()
+		end, { desc = "Toggle GitHub Copilot CLI" })
+
+		vim.keymap.set("n", "<leader>tt", function()
+			term:toggle()
+		end, { desc = "Toggle terminal" })
+	end,
 }
