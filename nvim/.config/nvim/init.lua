9fba1decaa58b1017e7eb1781887274afb3b428b
Bendik Svendsen <bendik.svendsen@fremtind.no>
2026-05-13 13:56:06 +0200

    nvim: keymaps and auto-reload

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
+require("keymaps")
 
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
@@ -9,6 +10,13 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function()
 		vim.cmd("only")
 		vim.bo.buflisted = true
+	end,
+})
+
+-- auto-reload files changed outside of nvim
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+	callback = function()
+		vim.cmd("silent! checktime")
 	end,
 })
 
@@ -23,77 +31,8 @@ vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.signcolumn = "yes"
 vim.o.foldenable = false
+vim.o.autoread = true
 vim.wo.relativenumber = true
-
---KEYMAPS
-vim.keymap.set("n", "<Tab>", function()
-	require("oil").open()
-end)
-
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { silent = true })
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { silent = true })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
-
---unhighlight
-vim.keymap.set("n", "<leader>h", ":noh<CR>", { silent = true })
-
---terminal
-vim.keymap.set("t", "<S-Esc>", [[<C-\><C-n>]])
-
---saving&quitting
-vim.keymap.set("n", "<C-s>", ":w<CR>")
-vim.keymap.set("n", "<F5>", ":wa<CR>")
-vim.keymap.set("n", "<BS>", ":confirm bdelete<CR>")
-vim.keymap.set("n", "<C-BS>", ":qa<CR>")
-
---copilot
---this is necessary to still allow default tab behavior when copilot suggestion is not visible
-vim.keymap.set("i", "<Tab>", function()
-	local copilot = require("copilot.suggestion")
-	if copilot.is_visible() then
-		copilot.accept_line()
-	else
-		return "\t"
-	end
-end, { expr = true })
-
---telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>b", builtin.buffers, {})
-
---harpoon
-local harpoon = require("harpoon")
-harpoon:setup()
-
-vim.keymap.set("n", "<leader>a", function()
-	harpoon:list():add()
-end)
-vim.keymap.set("n", "<leader>e", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
-
-vim.keymap.set("n", "<C-h>", function()
-	harpoon:list():select(1)
-end)
-vim.keymap.set("n", "<C-j>", function()
-	harpoon:list():select(2)
-end)
-vim.keymap.set("n", "<C-k>", function()
-	harpoon:list():select(3)
-end)
-vim.keymap.set("n", "<C-l>", function()
-	harpoon:list():select(4)
-end)
-
-vim.keymap.set("n", "<C-,>", function()
-	harpoon:list():prev()
-end)
-vim.keymap.set("n", "<C-.>", function()
-	harpoon:list():next()
-end)
 
 local treesitter = require("treesitter.treesitter_setup")
 treesitter.setup()
