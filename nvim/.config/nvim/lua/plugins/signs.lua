7224d15fdcb818687b17c365c64b80753aa57d74
Bendik Svendsen <bendik.svendsen@fremtind.no>
2026-05-15 12:45:33 +0200

    nvim: add signs

--- nvim/.config/nvim/lua/plugins/signs.lua
+++ nvim/.config/nvim/lua/plugins/signs.lua
@@ -1,4 +1,10 @@
 return {
-	{ "lewis6991/gitsigns.nvim", opts = {} },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle Git Blame" },
+		},
+	},
 	{ "nvim-tree/nvim-web-devicons", opts = {} },
 }
