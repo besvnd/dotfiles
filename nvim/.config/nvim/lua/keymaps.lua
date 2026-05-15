530b151c4c2ec9719336e851c0bec4da9599d423
Bendik Svendsen <bendik.svendsen@fremtind.no>
2026-05-16 00:08:14 +0200

    nvim: add hop

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,5 +1,7 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
 
 vim.keymap.set("n", "<Tab>", function()
 	require("oil").open()
