be3a61c7057f505e53dde2c886e4ed643fbbef34
Bendik Svendsen <bendik.svendsen@fremtind.no>
2026-05-29 12:46:55 +0200

    nvim: rewrite config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,42 +1,61 @@
-require("config.lazy")
-require("keymaps")
-
-vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
-
---help files open in full window and are listed in buffer elements
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "help",
-	callback = function()
-		vim.cmd("only")
-		vim.bo.buflisted = true
-	end,
-})
-
--- auto-reload files changed outside of nvim
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
-	callback = function()
-		vim.cmd("silent! checktime")
-	end,
-})
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
 vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.signcolumn = "yes"
-vim.o.foldenable = false
-vim.o.autoread = true
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
 
-local treesitter = require("treesitter.treesitter_setup")
-treesitter.setup()
+-- plugins
+vim.pack.add({
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/vague2k/vague.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/nvim-mini/mini.pick" },
+  { src = "https://github.com/saghen/blink.lib" },
+  {	src = "https://github.com/saghen/blink.cmp", version = 'v1' , },
+  { src = "https://github.com/kdheepak/lazygit.nvim" },
+}) 
 
--- colorscheme
-vim.cmd([[colorscheme catppuccin-macchiato]])
+-- keymaps
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
+vim.keymap.set("n", "<leader>gg", '<cmd>LazyGit<CR>')
+vim.keymap.set("n", '<leader>e', function()
+  if vim.bo.filetype == 'oil' then
+    require("oil.actions").close.callback()
+  else
+    vim.cmd('Oil')
+  end
+end)
+
+require "mini.pick".setup()
+require "oil".setup()
+vim.lsp.enable("jdtls")
+
+-- autocomplete
+vim.api.nvim_create_autocmd("InsertEnter", {
+	pattern = "*",
+	group = group,
+	once = true,
+	callback = function()
+		require("blink.cmp").setup({
+			keymap = { preset = "super-tab" },
+			appearance = {
+				nerd_font_variant = "mono",
+				use_nvim_cmp_as_default = true,
+			},
+			completion = {
+				documentation = { auto_show = false },
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		})
+	end,
+})
+
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
