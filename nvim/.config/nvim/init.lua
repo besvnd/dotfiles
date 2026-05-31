9cad7d077d74de2b41e864d6e1d537d0d7cea17a
Bendik Svendsen <bendik.svendsen@fremtind.no>
2026-05-31 10:33:14 +0200

    nvim: add tree-sitter-manager

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,6 +20,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
   { src = "https://github.com/nvim-lualine/lualine.nvim" },
   { src = "https://github.com/GooseRooster/cairn.nvim" },
+  { src = "https://github.com/romus204/tree-sitter-manager.nvim" }
 }) 
 
 -- keymaps
@@ -71,6 +72,20 @@ require "oil".setup()
 -- lsp setup
 vim.lsp.enable("jdtls")
 
+-- cairn setup
+require "cairn".setup()
+
+-- tree-sitter-manager setup
+require("tree-sitter-manager").setup({
+  -- Default Options
+  -- ensure_installed = {}, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
+  -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+  -- auto_install = false, -- if enabled, install missing parsers when editing a new file
+  -- highlight = true, -- treesitter highlighting is enabled by default
+  -- languages = {}, -- override or add new parser sources
+})
+
+
 -- autocomplete
 vim.api.nvim_create_autocmd("InsertEnter", {
 	pattern = "*",
@@ -93,9 +108,6 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 		})
 	end,
 })
-
--- cairn setup
-require "cairn".setup()
 
 
 -- themes etc 
