b21e92b0a110a18785e8628a2592c3e6494caf5e
Bendik Svendsen <bendik.svendsen@fremtind.no>
2026-06-01 23:14:04 +0200

    nvim: add flash

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -21,7 +21,8 @@ vim.pack.add({
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
   { src = "https://github.com/nvim-lualine/lualine.nvim" },
   { src = "https://github.com/GooseRooster/cairn.nvim" },
-  { src = "https://github.com/romus204/tree-sitter-manager.nvim" }
+  { src = "https://github.com/romus204/tree-sitter-manager.nvim" },
+  { src = "https://github.com/folke/flash.nvim" }
 }) 
 
 -- keymaps
@@ -97,6 +98,14 @@ require("tree-sitter-manager").setup({
   -- languages = {}, -- override or add new parser sources
 })
 
+
+-- flash setup
+require("flash").setup()
+vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
 
 -- autocomplete
 vim.api.nvim_create_autocmd("InsertEnter", {
